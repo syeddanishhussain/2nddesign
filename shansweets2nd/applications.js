c989a375e186f0a3df9134f0adb154989281c0c0
Danish hussain <syeddanish_hussain@hotmail.com>
2016-01-24 18:44:09 -0500

    at home

--- shansweets2nd/applications.js
+++ shansweets2nd/applications.js
@@ -2,6 +2,10 @@
  * Created by dell1 on 14/01/2016.
  */
 angular.module("applications", ["ngMaterial", "ngMdIcons"])
+    .config(function($mdIconProvider) {
+        $mdIconProvider
+            .iconSet('communication', 'img/icons/sets/communication-icons.svg', 24);
+    })
 //var Firebase = require("firebase");
 //var myFirebaseRef = new Firebase("https://shaansweets.firebaseio.com/")
 
@@ -36,6 +40,28 @@ angular.module("applications", ["ngMaterial", "ngMdIcons"])
             $mdDialog.show({
                 controller: DialogController,
                 templateUrl: 'dialog2.tmpl.html',
+                parent: angular.element(document.body),
+                targetEvent: ev,
+                clickOutsideToClose: true,
+                fullscreen: useFullScreen
+            })
+                .then(function (answer) {
+                    $scope.status = 'You said the information was "' + answer + '".';
+                }, function () {
+                    $scope.status = 'You cancelled the dialog.';
+                });
+            $scope.$watch(function () {
+                return $mdMedia('xs') || $mdMedia('sm');
+            }, function (wantsFullScreen) {
+                $scope.customFullscreen = (wantsFullScreen === true);
+            });
+        };
+
+        $scope.showAdvanced2 = function (ev) {
+            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs')) && $scope.customFullscreen;
+            $mdDialog.show({
+                controller: DialogController,
+                templateUrl: 'chat.html',
                 parent: angular.element(document.body),
                 targetEvent: ev,
                 clickOutsideToClose: true,
