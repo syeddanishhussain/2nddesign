024e50a3f39593c5c310d8f682490c1149c7e7d1
Danish hussain <syeddanish_hussain@hotmail.com>
2016-01-26 04:47:19 -0500

    danish

--- shansweets2nd/applications.js
+++ shansweets2nd/applications.js
@@ -2,12 +2,12 @@
  * Created by dell1 on 14/01/2016.
  */
 angular.module("applications", ["ngMaterial", "ngMdIcons"])
-    
+
 //var Firebase = require("firebase");
 //var myFirebaseRef = new Firebase("https://shaansweets.firebaseio.com/")
 
 
-    .controller("applicationController", function ($scope, $mdDialog, $mdMedia) {
+    .controller("applicationController", function ($scope, $mdDialog, $mdMedia, $timeout, $mdSidenav, $log) {
         $scope.status = '  ';
         $scope.customFullscreen = $mdMedia('xs') || $mdMedia('sm');
 
@@ -75,6 +75,66 @@ angular.module("applications", ["ngMaterial", "ngMdIcons"])
                 $scope.customFullscreen = (wantsFullScreen === true);
             });
         };
+        $scope.toggleLeft = buildDelayedToggler('left');
+        $scope.toggleRight = buildToggler('right');
+        $scope.isOpenRight = function(){
+            return $mdSidenav('right').isOpen();
+        };
+        /**
+         * Supplies a function that will continue to operate until the
+         * time is up.
+         */
+        function debounce(func, wait, context) {
+            var timer;
+            return function debounced() {
+                var context = $scope,
+                    args = Array.prototype.slice.call(arguments);
+                $timeout.cancel(timer);
+                timer = $timeout(function() {
+                    timer = undefined;
+                    func.apply(context, args);
+                }, wait || 10);
+            };
+        }
+        /**
+         * Build handler to open/close a SideNav; when animation finishes
+         * report completion in console
+         */
+        function buildDelayedToggler(navID) {
+            return debounce(function() {
+                $mdSidenav(navID)
+                    .toggle()
+                    .then(function () {
+                        $log.debug("toggle " + navID + " is done");
+                    });
+            }, 200);
+        }
+        function buildToggler(navID) {
+            return function() {
+                $mdSidenav(navID)
+                    .toggle()
+                    .then(function () {
+                        $log.debug("toggle " + navID + " is done");
+                    });
+            }
+        }
+    })
+    .controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+        $scope.close = function () {
+            $mdSidenav('left').close()
+                .then(function () {
+                    $log.debug("close LEFT is done");
+                });
+        };
+    })
+    .controller('RightCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+        $scope.close = function () {
+            $mdSidenav('right').close()
+                .then(function () {
+                    $log.debug("close RIGHT is done");
+                });
+        };
+
     });
 function DialogController($scope, $mdDialog) {
     $scope.hide = function () {
